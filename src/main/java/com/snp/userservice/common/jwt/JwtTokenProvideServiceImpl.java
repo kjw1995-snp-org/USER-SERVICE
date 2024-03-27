fd3bd47f269877ee62aed7ea5fe161ac06d4c27e
kimjungwoo <wjdddn312@hellofunding.co.kr>
2024-03-27 14:48:25 +0900

    JWT 설정 변경

--- src/main/java/com/snp/userservice/common/jwt/JwtTokenProvideServiceImpl.java
+++ src/main/java/com/snp/userservice/common/jwt/JwtTokenProvideServiceImpl.java
@@ -3,6 +3,7 @@ package com.snp.userservice.common.jwt;
 
 import com.snp.userservice.jpa.entity.Member;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -33,9 +34,9 @@ public class JwtTokenProvideServiceImpl implements JwtTokenProvideService,Initia
     private Key key;
 
     public JwtTokenProvideServiceImpl(
-            @Value("${spring.jwt.secret-key}") String secret,
-            @Value("${spring.jwt.access-token-expiry-seconds}") long accessTokenExpirySeconds,
-            @Value("${spring.jwt.refresh-token-expiry-seconds}") long refreshTokenExpirySeconds) {
+            @Value("${jwt.secret-key}") String secret,
+            @Value("${jwt.access-token-expiry-seconds}") long accessTokenExpirySeconds,
+            @Value("${jwt.refresh-token-expiry-seconds}") long refreshTokenExpirySeconds) {
 
         this.secret = secret;
         this.accessTokenExpiryMilliseconds = accessTokenExpirySeconds * 1000;
@@ -60,10 +61,21 @@ public class JwtTokenProvideServiceImpl implements JwtTokenProvideService,Initia
         Claims claims = Jwts.claims(privateClaimsMap).setAudience(member.getName()).setExpiration(expiryDate);
 
         return Jwts.builder()
+                   .setHeader(createHeader())
                    .setClaims(claims)
                    .signWith(key, SignatureAlgorithm.HS512)
                    .compact();
 
+    }
+
+    private Map<String, Object> createHeader() {
+
+        Map<String, Object> header = new HashMap<>();
+
+        header.put("typ", "JWT");
+        header.put("alg", "HS512");
+
+        return header;
     }
 
 }
